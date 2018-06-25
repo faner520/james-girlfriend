fa327564d32a20b2626a121b924257528ad70535
KillaKilla8 <clarkanderson346@gmail.com>
2018-06-24 18:58:25 -0700

    Edited js file

--- mygirlfriend.js
+++ mygirlfriend.js
@@ -1,1 +1,10 @@
-window.onload=function(){alert("Hello James");};
+window.onload=function(){alert("Hello James");
+document.getElementsByTagName("form")[0].onclick=respond;
+};
+
+
+
+
+
+
+function respond(){alert("Ok cool!");}
