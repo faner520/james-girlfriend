47fedffa52b8e92779402b2a51cfde3d699330b5
bull313 <=>
2018-06-24 20:02:25 -0700

    Feature 2

--- mygirlfriend.js
+++ mygirlfriend.js
@@ -7,7 +7,23 @@ function respond() {
 	var input = document.getElementsByTagName("input")[1].value;
 	
 	if (input == "How are you doing?") {
-		alert("I am good");
+		var num = Math.random() * 100;
+		num = num / 1;
+		if (num < 20) {
+			alert("I'm okay");
+		}
+		else if (num < 60) {
+			alert("I'm great!");
+		}
+		else if (num < 80) {
+			alert("I'm alright");
+		}
+		else if (num < 90) {
+			alert("I'm not feeling that great");
+		}
+		else {
+			alert("I'm okay, thanks for asking!");
+		}
 	}
 	else {
 		alert("wtf");
