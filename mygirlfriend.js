e6c0daeaafd416f55fcf19c7f7a51d96ed40c5b0
bull313 <=>
2018-06-24 19:44:42 -0700

    Feature 1

--- mygirlfriend.js
+++ mygirlfriend.js
@@ -1,10 +1,17 @@
-window.onload=function(){alert("James Sucks");
-document.getElementsByTagName("form")[0].onclick=respond;
+window.onload=function() {
+	alert("James Sucks");
+	document.getElementsByTagName("form")[0].onsubmit=respond;
 };
 
-
-
-
-
-
-function respond(){alert("Ok cool!");}
+function respond() {
+	var input = document.getElementsByTagName("input")[1].value;
+	
+	if (input == "How are you doing?") {
+		alert("I am good");
+	}
+	else {
+		alert("wtf");
+	}
+	
+	return false;
+}
